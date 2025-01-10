3de07c36583ca184134ce4eb9cf691c5670f7c03
git <36236750+szerookii@users.noreply.github.com>
2025-01-10 20:28:39 +0100

    remove logging

--- server/plugins/reading-time.ts
+++ server/plugins/reading-time.ts
@@ -2,7 +2,6 @@ import { visit } from 'unist-util-visit'
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('content:file:afterParse', (file: any) => {
-    console.log('file', file)
     if (file._id.endsWith('.md') && !file.wordCount) {
       file.wordCount = 0;
       visit(file.body, (n: any) => n.type === 'text', (node) => {
